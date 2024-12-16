b3687270020a1f7eabf935ec377a8e0c00763b50
etiyoungreis <etiyoungreis@gmail.com>
2024-12-16 19:09:47 +0200

    Revert "Merge branch 'main' of https://github.com/eti-youngreis/image_design-frontend"
    This reverts commit a46bbf7c4790f3da4a8d24aaa1809b8279a346c6, reversing
    changes made to c500ebe33753d6a026f475a9dee7fea8f81e2ae7.

--- image-design/src/redux/image/image.slice.ts
+++ image-design/src/redux/image/image.slice.ts
@@ -6,20 +6,12 @@ const imagesSlice = createSlice({
     initialState:[],
     reducers: {
         setImages: (state:ImageType[], action: PayloadAction<ImageType[]>) => {
-<<<<<<< HEAD
             state.length=0
             state.push(...action.payload)
-=======
-            state = action.payload
->>>>>>> 51a29fb1c023f134593ddabb8f6b8f9776eb0b94
         }
     }
 })
 
 export const { setImages } = imagesSlice.actions
 
-<<<<<<< HEAD
 export default imagesSlice.reducer
-=======
-export default imagesSlice.reducer
->>>>>>> 51a29fb1c023f134593ddabb8f6b8f9776eb0b94
